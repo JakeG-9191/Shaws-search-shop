32d810941080804a0f5f3c7fe517db1bb0995926
JakeG-9191 <jacobgarlick@msn.com>
2020-05-22 13:16:37 -0600

    first commit

---
+++ server.js
@@ -0,0 +1,58 @@
+// Require dotenv to be installed
+require('dotenv').config();
+// Grab necessary keys for Spotify from directory
+var keys = require('./keys.js');
+// Axios requirement
+var axios = require('axios');
+// Spotify Requirement
+var Spotify = require('node-spotify-api');
+
+const userInput = 'Clutch';
+
+console.log('this is on');
+
+const concertRequest = () => {
+  axios
+    .get(
+      'https://rest.bandsintown.com/artists/' +
+        userInput +
+        '/events?app_id=codingbootcamp'
+    )
+    .then(function (response) {
+      for (var i = 0; i < 5; i++) {
+        var time = moment(response.data[i].datetime).format('L');
+        console.log('\n------------Next Event----------------');
+        console.log('Venue Name: ' + response.data[i].venue.name);
+        console.log('Venue Country: ' + response.data[i].venue.country);
+        console.log('Venue City: ' + response.data[i].venue.city);
+        console.log(time);
+      }
+    })
+    .catch(function (error) {
+      console.log(error);
+    });
+};
+
+const spotify = new Spotify(keys.spotify);
+
+const spotifyRequest = () => {
+  spotify
+    .search({
+      type: 'track',
+      query: userInput || 'The Sign Ace of Base',
+      limit: 1,
+    })
+    .then(function (response) {
+      console.log('\n-------Artist Name----------');
+      console.log(response.tracks.items[0].artists[0].name);
+      console.log('-------Song Name----------');
+      console.log(response.tracks.items[0].name);
+      console.log('--------Preview URL---------');
+      console.log(response.tracks.items[0].preview_url);
+      console.log('--------Album Name---------');
+      console.log(response.tracks.items[0].album.name);
+    })
+    .catch(function (err) {
+      console.log(err);
+    });
+};
