b1c3ff16011c51f30a141e92dbd7ce4271a10805
JakeG-9191 <jacobgarlick@msn.com>
2020-07-29 10:28:13 -0600

    successfully scraped data from John Hopkins covid site

--- server.js
+++ server.js
@@ -1,7 +1,30 @@
 import cheerio from 'cheerio';
 import puppeteer from 'puppeteer';
 
-const url = 'https://www.reddit.com/r/harrypotter/';
+// const url = 'https://www.reddit.com/r/harrypotter/';
+
+// puppeteer
+//   .launch()
+//   .then((browser) => browser.newPage())
+//   .then((page) => {
+//     return page.goto(url).then(function () {
+//       return page.content();
+//     });
+//   })
+//   .then((html) => {
+//     const $ = cheerio.load(html);
+//     const newsHeadlines = [];
+//     $('a[href*="/r/harrypotter/comments"] > div > h3').each(function () {
+//       newsHeadlines.push({
+//         title: $(this).text(),
+//       });
+//     });
+
+//     console.log(newsHeadlines);
+//   })
+//   .catch(console.error);
+
+const url = 'https://coronavirus.jhu.edu/';
 
 puppeteer
   .launch()
@@ -14,7 +37,7 @@ puppeteer
   .then((html) => {
     const $ = cheerio.load(html);
     const newsHeadlines = [];
-    $('a[href*="/r/harrypotter/comments"] > div > h3').each(function () {
+    $('article > ul > li > figure').each(function () {
       newsHeadlines.push({
         title: $(this).text(),
       });
