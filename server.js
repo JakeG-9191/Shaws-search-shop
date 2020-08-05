fada5238b22f9db260f2bc4f9a34533df46f6501
JakeG-9191 <jacobgarlick@msn.com>
2020-08-05 11:27:56 -0600

    finalized logic to pull values from john hopkins using puppeteer

--- server.js
+++ server.js
@@ -7,29 +7,6 @@ import fs from 'fs';
 const error = chalk.bold.red;
 const success = chalk.keyword('green');
 
-// const url = 'https://coronavirus.jhu.edu/';
-
-// puppeteer
-//   .launch({ headless: false })
-//   .then((browser) => browser.newPage())
-//   .then((page) => {
-//     return page.goto(url).then(function () {
-//       return page.content();
-//     });
-//   })
-//   .then((html) => {
-//     const $ = cheerio.load(html);
-//     const newsHeadlines = [];
-//     $('article > ul > li > figure').each(function () {
-//       newsHeadlines.push({
-//         title: $(this).text(),
-//       });
-//     });
-
-//     return console.log(newsHeadlines);
-//   })
-//   .catch(console.error);
-
 (async () => {
   try {
     const browser = await puppeteer.launch({ headless: false });
@@ -39,14 +16,14 @@ const success = chalk.keyword('green');
 
     const newFigures = await page.evaluate(() => {
       let allnewFigures = document.querySelectorAll(
-        `ul.FeaturedStats_stats__2keLP`
+        `li.FeaturedStats_stat__1MPv_`
       );
       let updatedTitle = document.querySelectorAll(`figure > figcaption`);
       let updatedNumbers = document.querySelectorAll(`figure > p`);
       let figureArray = [];
       for (let i = 0; i < allnewFigures.length; i++) {
         figureArray[i] = {
-          info: allnewFigures[i].innerText.trim(),
+          // info: allnewFigures[i].innerText.trim(),
           title: updatedTitle[i].innerHTML.trim(),
           nums: updatedNumbers[i].innerHTML.trim(),
         };
@@ -63,39 +40,39 @@ const success = chalk.keyword('green');
   }
 })();
 
-// (async () => {
-//   try {
-//     const browser = await puppeteer.launch({ headless: false });
-//     const page = await browser.newPage();
-//     await page.goto(`https://news.ycombinator.com/`);
-//     await page.waitForSelector(`a.storylink`);
+(async () => {
+  try {
+    const browser = await puppeteer.launch({ headless: false });
+    const page = await browser.newPage();
+    await page.goto(`https://news.ycombinator.com/`);
+    await page.waitForSelector(`a.storylink`);
 
-//     const news = await page.evaluate(() => {
-//       let titleNodeList = document.querySelectorAll(`a.storylink`);
-//       let scoreList = document.querySelectorAll(`td.subtext`);
-//       let titleLinkArray = [];
-//       for (let i = 0; i < titleNodeList.length; i++) {
-//         titleLinkArray[i] = {
-//           title: titleNodeList[i].innerText.trim(),
-//           link: titleNodeList[i].getAttribute(`href`),
-//           score: scoreList[i].innerText.trim(),
-//         };
-//       }
-//       return titleLinkArray;
-//     });
-//     await browser.close();
-//     // console.log(news);
-//     fs.writeFile('hackernews.json', JSON.stringify(news), function (err) {
-//       if (err) throw err;
-//       console.log('Saved');
-//     });
-//     console.log(success('browser closed'));
-//   } catch (err) {
-//     console.log(error(err));
-//     await browser.close();
-//     console.log(error('browser closed'));
-//   }
-// })();
+    const news = await page.evaluate(() => {
+      let titleNodeList = document.querySelectorAll(`a.storylink`);
+      let scoreList = document.querySelectorAll(`td.subtext`);
+      let titleLinkArray = [];
+      for (let i = 0; i < titleNodeList.length; i++) {
+        titleLinkArray[i] = {
+          title: titleNodeList[i].innerText.trim(),
+          link: titleNodeList[i].getAttribute(`href`),
+          score: scoreList[i].innerText.trim(),
+        };
+      }
+      return titleLinkArray;
+    });
+    await browser.close();
+    console.log(news);
+    // fs.writeFile('hackernews.json', JSON.stringify(news), function (err) {
+    //   if (err) throw err;
+    //   console.log('Saved');
+    // });
+    console.log(success('browser closed'));
+  } catch (err) {
+    console.log(error(err));
+    await browser.close();
+    console.log(error('browser closed'));
+  }
+})();
 
 // const url =
 //   'https://www.ticketmaster.com/search?radius=75&sort=date%2Casc&unit=miles&tab=events&daterange=all';
