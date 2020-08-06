52633b71a312d4c23a50a6664cca970301fd66b5
JakeG-9191 <jacobgarlick@msn.com>
2020-08-06 13:24:24 -0600

    successful scrapes of all boating equipment at dr, all scrapes currenlty run and save in under 30 seconds

--- dr_scrape.js
+++ dr_scrape.js
@@ -20,22 +20,63 @@ export async function downRiver() {
       let updatedTitle = document.querySelectorAll(
         `div.item-mask > div.item-caption > h5`
       );
-      //   let updatedNumbers = document.querySelectorAll(`figure > p`);
       let figureArray = [];
       for (let i = 0; i < allnewFigures.length; i++) {
         figureArray[i] = {
           title: updatedTitle[i].innerHTML.trim(),
-          //   nums: updatedNumbers[i].innerHTML.trim(),
         };
       }
       return figureArray;
     });
     await browser.close();
-    console.log(dRScrape);
-    // fs.writeFile('jhucovid.json', JSON.stringify(newFigures), function (err) {
-    //   if (err) throw err;
-    //   console.log('Saved New File Successfully - JHU');
-    // });
+    // console.log(dRScrape);
+    fs.writeFile('dRstaff.json', JSON.stringify(dRScrape), function (err) {
+      if (err) throw err;
+      console.log('Saved New File Successfully - DR Staff');
+    });
+    console.log(success('Browser Closed'));
+  } catch (err) {
+    console.log(error(err));
+    await browser.close();
+    console.log(error('Browser Closed With Error!'));
+  }
+}
+
+export async function downRiverDouble() {
+  console.log(success('Down River Double Scrape Starting'));
+  try {
+    const browser = await puppeteer.launch({ headless: false });
+    const page = await browser.newPage();
+    await page.goto(`https://www.downriverequip.com/boats-cid-2`);
+    await page.waitForSelector(`div.item-box`);
+
+    const dRDoubleScrape = await page.evaluate(() => {
+      let itemOverallInfo = document.querySelectorAll(`div.nf-item`);
+      let itemName = document.querySelectorAll(
+        `div.item-box > div.shop-item-info > a > h6.shop-item-name`
+      );
+      let itemBrand = document.querySelectorAll(
+        `div.shop-item-info > a > h6.shop-item-brand`
+      );
+      let itemPrice = document.querySelectorAll(`div.shop-item-price`);
+      let figureArray = [];
+      for (let i = 0; i < itemOverallInfo.length; i++) {
+        figureArray[i] = {
+          name: itemName[i].innerText.trim(),
+          brand: itemBrand[i].innerText.trim(),
+          price: itemPrice[i].innerText.trim(),
+        };
+      }
+      return figureArray;
+    });
+    await browser.close();
+    // console.log(dRDoubleScrape);
+    fs.writeFile('dRboats.json', JSON.stringify(dRDoubleScrape), function (
+      err
+    ) {
+      if (err) throw err;
+      console.log('Saved New File Successfully - DR Boats');
+    });
     console.log(success('Browser Closed'));
   } catch (err) {
     console.log(error(err));
@@ -46,5 +87,6 @@ export async function downRiver() {
 
 export function launchDR() {
   downRiver();
-  return console.log(`launched Down River Scrape`);
+  downRiverDouble();
+  return console.log(`launched Down River Scrape & DR Double Scrape`);
 }
