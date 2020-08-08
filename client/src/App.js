6f5eb99fa8bd45c1f08e6f91b5cc3fe9838b79b0
JakeG-9191 <jacobgarlick@msn.com>
2020-08-07 19:37:19 -0600

    reworked logic to push new arrays for all data imported from json, then arrays of info are mapped for display

--- client/src/App.js
+++ client/src/App.js
@@ -2,36 +2,66 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import jhuCovid from '../src/json/jhucovid.json';
+import hackernews from '../src/json/hackernews.json';
 
 class App extends Component {
   state = {
-    title: [],
-    nums: '',
-    length: '',
+    info: [],
+    info2: [],
   };
 
   callAPI() {
+    let myArray = [];
+    for (let i = 0; i < jhuCovid.length; i++) {
+      let newTitle = jhuCovid[i].title;
+      let nemNum = jhuCovid[i].nums;
+      myArray.push(newTitle, nemNum);
+    }
     this.setState({
-      length: jhuCovid.length,
-      title: jhuCovid.title,
+      info: myArray,
+    });
+  }
+
+  callAPI2() {
+    let myArray = [];
+    for (let i = 0; i < hackernews.length; i++) {
+      let newTitle = hackernews[i].title;
+      let nemLink = hackernews[i].link;
+      let newScore = hackernews[i].score;
+      myArray.push(newTitle, nemLink, newScore);
+    }
+    this.setState({
+      info2: myArray,
     });
   }
 
   componentDidMount() {
     this.callAPI();
+    this.callAPI2();
   }
 
   render() {
     return (
-      <div className='App'>
-        <header className='App-header'>
-          <img src={logo} className='App-logo' alt='logo' />
-          <h1 className='App-title'>Welcome to the Thunder Dome</h1>
-        </header>
-        <p className='App-intro'>{this.state.length}</p>
-        <p className='App-intro'>{this.state.title}</p>
-        <p className='App-intro'>{this.state.nums}</p>
-      </div>
+      <>
+        <div className='App'>
+          <header className='App-header'>
+            <img src={logo} className='App-logo' alt='logo' />
+            <h1 className='App-title'>Welcome to the Thunder Dome</h1>
+          </header>
+          <p className='App-intro'>{this.state.length}</p>
+          <p className='App-intro'>
+            {this.state.info.map((info) => (
+              <p>{info}</p>
+            ))}
+          </p>
+          <hr />
+          <div className='App-intro'>
+            {this.state.info2.map((info) => (
+              <p>{info}</p>
+            ))}
+          </div>
+        </div>
+      </>
     );
   }
 }
